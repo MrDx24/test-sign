247791b9b3cd402a31386b19b7edbd47a2724321
Deep Patadiya <deeppatadiya24@gmail.com>
2024-03-20 11:51:58 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AngularSignaturePadModule, SignaturePadComponent } from '@almothafar/angular-signature-pad';
 import { CommonModule } from '@angular/common';
+// @ts-ignore
+import html2pdf from 'html2pdf.js';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-root',
@@ -11,32 +14,87 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'signature';
 
   printbill:boolean = false;
   value:any
 
   image = new Image();
+  image1 = new Image();
+
   @ViewChild('divToPrint', { static: false }) divToPrint!: ElementRef;
   @ViewChild('sign', { static: false }) sign!: SignaturePadComponent;
-  @ViewChild('sign', { static: false }) sign1!: ElementRef;
+  @ViewChild('sign1', { static: false }) sign1!: SignaturePadComponent;
 
   constructor() { }
 
+  @ViewChild('divToPrint') content!: ElementRef;
 
+
+  generatePDF(): void {
+
+    this.done();
+    const DATA = this.content.nativeElement;
+    const doc: jsPDF = new jsPDF({ unit: 'in', format: 'A4', orientation: 'portrait'});
+    doc.html(DATA, {
+       callback: (doc) => {
+         doc.output("dataurlnewwindow",{"filename" : "sample"});
+       }
+    });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  convertToPDF() {
+    this.done();
+    const element = this.content.nativeElement;
+    const opt = {
+      margin: [-0.6,0,0,0],
+      filename: 'my_document.pdf',
+      image: { type: 'png', quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'in', format: 'A4', orientation: 'portrait', pyPDFEmbedFont:true }
+    };
+
+    // html2pdf().from(element).set(opt).output("dataurlnewwindow",{"filename" : "sample"});
+    html2pdf().from(element).set(opt).outputPdf('datauristring');
+  }
 
 
   printDiv() {
 
+
     this.done();
-    setTimeout(() => {
-      let printContents = this.divToPrint.nativeElement.innerHTML;
-      let originalContents = document.body.innerHTML;
-      document.body.innerHTML = printContents;
-      window.print();
-      document.body.innerHTML = originalContents;
-    }, 1);
+    // setTimeout(() => {
+    //   let printContents = this.divToPrint.nativeElement.innerHTML;
+    //   let originalContents = document.body.innerHTML;
+    //   document.body.innerHTML = printContents;
+    //   window.print();
+    //   document.body.innerHTML = originalContents;
+    // }, 1);
   }
+
 
   clear() {
     this.printbill = false;
@@ -48,7 +106,13 @@ export class AppComponent {
 
     const signatureData = this.sign.toDataURL(); // Get the signature in Base64 format
     this.image.src = signatureData;
+    const signatureData1 = this.sign1.toDataURL(); // Get the signature in Base64 format
+    this.image1.src = signatureData1;
 
+
+  }
+
+  docs() {
 
   }
 }
